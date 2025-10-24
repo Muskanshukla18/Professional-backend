ef32225142e7bc7b7e5da758e75195b37ef97c8d
MuskanShukla <shuklamuskan979@gmail.com>
2025-10-24 19:11:53 +0530

    Add user account management endpoint

---
+++ src/models/subscription.model.js
@@ -0,0 +1,31 @@
+
+import mongoose,{Schema} from "mongoose";
+
+const SubscriptionSchema = new Schema({
+
+ subscriber:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:"User"
+ },
+ channel:{
+   type:mongoose.Schema.Types.ObjectId,
+   ref:"User"
+ }
+ 
+},  {timestamps:true}
+)
+
+export const Subscription= mongoose.model("Subscription",SubscriptionSchema)
+
+
+
+
+
+
+
+
+
+
+
+
+
